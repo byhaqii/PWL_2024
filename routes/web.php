4e248c234f563b353b6bbd9d8797d52805c9ffc6
byhaqii <rizalbaihaqiiiiii@gmail.com>
2025-02-24 20:50:16 +0700

    Praktikum Route

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,19 @@ Route::get('/user/{name}', function ($name) {
     return 'Nama Saya '.$name;
 });
 
-// Bismillah
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+    });
+
+Route::get('/user/{name?}', function ($name='john') {
+    return 'Nama saya '.$name;
+    });
+
+Route::get('/user/profile', function() {
+    //
+})->name('profile');
+
